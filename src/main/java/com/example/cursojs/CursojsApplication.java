77a98ee284d7613ecbab5b58f943a74068c28bfe
algoridc <algoridc@gmail.com>
2021-09-07 11:11:44 +0200

    inicio del proyecto

---
+++ src/main/java/com/example/cursojs/CursojsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cursojs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cursojs/CursojsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursojs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
